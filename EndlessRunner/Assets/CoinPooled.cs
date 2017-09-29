ad62529ba7c9f8fddd9446210c8a0dbc55f19034
DandeOne <dande2@o2.pl>
2017-09-29 17:23:40 +0200

    Implemented object pooled coins and obstacles

---
+++ EndlessRunner/Assets/CoinPooled.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPooled : MonoBehaviour {
+    
+   public GameObject forward;
+   public GameObject left;
+   public GameObject right;
+
+	// Use this for initialization
+	void Start () {
+        forward.SetActive(false);
+        right.SetActive(false);
+        left.SetActive(false);
+        float prob = Random.Range(0f,1f);
+        if(prob<0.5)
+            forward.SetActive(true);
+         prob = Random.Range(0f, 1f); 
+        if (prob < 0.5)
+            right.SetActive(true);
+         prob = Random.Range(0f, 1f); 
+        if (prob < 0.5)
+            left.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
